471b62b6881e28d82251067034eeadb3c6a07053
Chris <106836962+Cujion@users.noreply.github.com>
2023-01-08 02:53:19 -0500

    setup server.js with required variables, setup schema.sql file to create employee database along with proper tables inside of database

---
+++ server.js
@@ -0,0 +1,9 @@
+const inquirer = require('inquirer');
+const prompt = inquirer.createPromptModule();
+const mysql = require('mysql2');
+require('console.table');
+
+const db = mysql.createConnection({
+    user: "root",
+    database: "employee_db"
+});
