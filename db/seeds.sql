ae20ca97a726f01b17d27880d5e5e50a553274cf
Chris <106836962+Cujion@users.noreply.github.com>
2023-01-08 17:46:16 -0500

    updated seeds.sql to use employee_db, created department names, created roles for department, created employee for department

---
+++ db/seeds.sql
@@ -0,0 +1,30 @@
+USE employee_db;
+
+INSERT INTO department (name)
+VALUES
+(Sales),
+(Engineering),
+(Finance),
+(Legal);
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Sales Lead', 100000, 1),
+('Salesperson', 80000, 1),
+('Lead Engineer', 150000, 2),
+('Software Engineer', 120000, 2),
+('Account Manager', 160000, 3),
+('Accountant', 125000, 3),
+('Legal Team Lead', 250000, 4),
+('Lawyer', 190000, 4)
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('John', 'Doe', 1, null),
+('Mike', 'Chan', 2, 'John Doe'),
+('Ashley', 'Rodriguez', 3, null),
+('Kevin', 'Tupik', 4, 'Ashley Rodriguez'),
+('Kunal', 'Singh', 5, null),
+('Malia', 'Brown', 6 'Kunal Singh'),
+('Sarah', 'Lourd', 7, null),
+('Tom', 'Allen', 8, 'Sarah Lourd')
