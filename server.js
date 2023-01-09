822343a92b63342cab751530b6ca2cab9380a4ce
Chris <106836962+Cujion@users.noreply.github.com>
2023-01-08 19:18:55 -0500

    added fancy console.log Employee Manager when prompt is ran

--- server.js
+++ server.js
@@ -10,13 +10,9 @@ const db = mysql.createConnection({
 
 const initialPrompt = (type) => {
     switch (type) {
-        case 'VIEW ALL EMPLOYEES': {
-            db.query('SELECT * FROM employee', (err, employees) => {
-                console.table(employees);
-                init();
-            });
+        case 'VIEW ALL EMPLOYEES':
+            viewAllDepartments();
             break;
-        }
         case 'ADD EMPLOYEE': {
             init();
             break;
@@ -25,24 +21,16 @@ const initialPrompt = (type) => {
             init();
             break;
         }
-        case 'VIEW ALL ROLES': {
-            db.query('SELECT * FROM role', (err, roles) => {
-                console.table(roles);
-                init();
-            });
+        case 'VIEW ALL ROLES':
+            viewAllRoles();
             break;
-        }
         case 'ADD ROLE': {
             init();
             break;
         }
-        case 'VIEW ALL DEPARTMENTS': {
-            db.query('SELECT * FROM department', (err, departments) => {
-                console.table(departments);
-                init();
-            });
+        case 'VIEW ALL DEPARTMENTS':
+            viewAllDepartments();
             break;
-        }
         case 'ADD DEPARTMENT': {
             init();
             break;
@@ -54,25 +42,61 @@ const initialPrompt = (type) => {
     }
 }
 
-const init = () => {
-prompt({
-    type: 'rawlist',
-    message: 'What would you like to do?',
-    choices: [
-        'VIEW ALL EMPLOYEES',
-        'ADD EMPLOYEE',
-        'UPDATE EMPLOYEE ROLE',
-        'VIEW ALL ROLES',
-        'ADD ROLE',
-        'VIEW ALL DEPARTMENTS',
-        'ADD DEPARTMENT',
-        'QUIT'
-    ],
-    name: 'type',
-})
-    .then((answers) => {
-        initialPrompt(answers.type);
+viewAllDepartments = () => {
+    db.query('SELECT * FROM employee', (err, employees) => {
+        console.table(employees);
+        init();
     });
+};
+
+viewAllRoles = () => {
+    db.query('SELECT * FROM role', (err, roles) => {
+        console.table(roles);
+        init();
+    });
+};
+
+viewAllDepartments = () => {
+    db.query('SELECT * FROM department', (err, departments) => {
+        console.table(departments);
+        init();
+    });
+};
+
+const init = () => {
+    console.info (`
+    ╔ ╗╔════╔═══╗═════╔╗══════════════════╗╔ ╗
+    ║ ║╚════║╔══╝═════║║══════════════════╝║ ║
+    ║ ║     ║╚══╦╗╔╦══╣║╔══╦╗─╔╦══╦══╗     ║ ║
+    ║ ║     ║╔══╣╚╝║╔╗║║║╔╗║║─║║║═╣║═╣     ║ ║
+    ║ ║     ║╚══╣║║║╚╝║╚╣╚╝║╚═╝║║═╣║═╣     ║ ║
+    ║ ║     ╚═══╩╩╩╣╔═╩═╩══╩═╗╔╩══╩══╝     ║ ║
+    ║ ║╔════╔═╗╔═╗═║║══════╔═╝║═══════════╗║ ║
+    ║ ║╚════║║╚╝║║═╚╝══════╚══╝═══════════╝║ ║
+    ║ ║     ║╔╗╔╗╠══╦═╗╔══╦══╦══╦═╗        ║ ║
+    ║ ║     ║║║║║║╔╗║╔╗╣╔╗║╔╗║║═╣╔╝        ║ ║
+    ║ ║     ║║║║║║╔╗║║║║╔╗║╚╝║║═╣║         ║ ║
+    ║ ║     ╚╝╚╝╚╩╝╚╩╝╚╩╝╚╩═╗╠══╩╝         ║ ║
+    ║ ║╔══════════════════╔═╝║════════════╗║ ║
+    ╚ ╝╚══════════════════╚══╝════════════╝╚ ╝`)
+    prompt({
+        type: 'rawlist',
+        message: 'What would you like to do?',
+        choices: [
+            'VIEW ALL EMPLOYEES',
+            'ADD EMPLOYEE',
+            'UPDATE EMPLOYEE ROLE',
+            'VIEW ALL ROLES',
+            'ADD ROLE',
+            'VIEW ALL DEPARTMENTS',
+            'ADD DEPARTMENT',
+            'QUIT'
+        ],
+        name: 'type',
+    })
+        .then((answers) => {
+            initialPrompt(answers.type);
+        });
 }
 
 init();
